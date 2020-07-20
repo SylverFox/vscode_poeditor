094bc8f10c9775d7c2b8975bf47de700b8a5e732
SylverFox <jorisdiesvelt@gmail.com>
2020-07-20 20:03:02 +0200

    Change project setup

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as poeditor from '@sylverfox/poeditor';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const console = vscode.window.createOutputChannel("POEditor");
+  console.show();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "poeditor" is now active!');
+  const authenticate = vscode.commands.registerCommand('poeditor.authenticate', () => {
+    vscode.window
+      .showInputBox({
+        placeHolder: "api token",
+        password: true,
+        ignoreFocusOut: true,
+      })
+      .then((api_token) => {
+        if (api_token) {
+          poeditor.projects.list(api_token);
+        }
+      });
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('poeditor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from POEditor!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(authenticate);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  //
+}
